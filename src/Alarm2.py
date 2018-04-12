0db13b1644139a9fea36d8ab5b20ee17965c86d7
Scott Walters <walte106@purdue.edu>
2018-04-11 20:28:42 -0400

    Updates to Group Push notifications

--- src/Alarm2.py
+++ src/Alarm2.py
@@ -37,7 +37,7 @@ def callFriend(phone, message):
 	print phone
 	print message
 	if message.find(" ") != -1:
-		message = message.replace(" ", "%20")
+	    message = message.replace(" ", "%20")
 	url_ = "http://45.56.125.90:5000/twiml/call/" + message
 	print url_
 	client.calls.create(to=phone, from_="+12603234126",url=url_)
@@ -87,7 +87,7 @@ def deleteAlarm(_id):
 	res = es.delete(index="alarm2", doc_type="doc", id=_id)
 	return jsonify(res)
 
-@app.route("/alarm/delete", methods=["GET"])
+@app.route("/delete", methods=["GET"])
 def deleteAlarmIndex():
 	es.indices.delete(index="alarm2")
 	es.indices.delete(index="token")
@@ -96,12 +96,6 @@ def deleteAlarmIndex():
 	es.indices.create(index="token")
 	es.indices.create(index="group")
 	return "DELETED"
-
-@app.route("/groupalarm", methods=["POST"])
-def createGroupAlarm():
-	res = es.index(index="groups", doc_type="doc", body=request.data)
-	retId = res["_id"]
-	return retId 
 
 @app.route("/token", methods=["POST"])
 def addToken():
@@ -115,25 +109,65 @@ def getAllTokens():
 	return jsonify(res)
 
 @app.route("/groups", methods=["GET"])
+##@crossdomain(origin="*")
 def getAllGroups():
 	res = es.search(index="group", doc_type="doc", body={"query": { "match_all": {}}})
 	return jsonify(res)
 
 @app.route("/group", methods=["POST"])
-def group():
+def createGroupAlarm():
 	res = es.index(index="group", doc_type="doc", body=request.data)
 	retId = res["_id"]
 	group = json.loads(request.data)
 	gList = group["members"]
-	user = es.search(index="token", doc_type="doc", body={"query": { "match": {"userId": gList[0] }}})
-	registration_id = user["hits"]["hits"][0]["_source"]["token"]
-	message_title = "Group Alarm Update"
-	message_body = "You have a group alarm update"
-	data_message = json.loads(request.data)
-	result = push_service.notify_single_device(registration_id=registration_id,
-	message_title=message_title, message_body=message_body, data_message=data_message)
-	print user
-	print result
-	return retId 
+	for i in range(len(gList)):
+	    user = es.search(index="token", doc_type="doc", body={"query": { "match": {"userId": gList[i] }}})
+	    registration_id = user["hits"]["hits"][0]["_source"]["token"]
+	    message_title = "New Group Alarm"
+	    message_body = "You have a group alarm update"
+	    data_message = json.loads(request.data)
+	    result = push_service.notify_single_device(registration_id=registration_id,
+	    message_title=message_title, message_body=message_body, data_message=data_message)
+	return res
+
+'''
+@app.route("/group/<_id>", methods=["PUT"])
+def updateGroupAlarm():
+	oldRes = es.search(index="group", doc_type="doc", body={"query": { "terms": {"_id": [_id]}}})
+	oldGroup = oldRes["hits"]["hits"][0]["_source"]
+	oldGList = oldGroup["members"]
+	newRes = es.index(index="group", doc_type="doc", id=_id,  body=request.data)
+	newGroup = json.loads(newRes)
+	newGList = newGroup["members"]
+	for i in range(len(gList)):
+	    user = es.search(index="token", doc_type="doc", body={"query": { "match": {"userId": gList[i] }}})
+	    
+	    registration_id = user["hits"]["hits"][0]["_source"]["token"]
+	    message_title = "Group Alarm Update"
+	    message_body = "You have a group alarm update"
+	    data_message = json.loads(request.data)
+	    result = push_service.notify_single_device(registration_id=registration_id,
+	    message_title=message_title, message_body=message_body, data_message=data_message)
+	return res
+	'''
+
+@app.route("/group/<_id>", methods=["DELETE"])
+def deleteGroupAlarm():
+	res = es.search(index="alarm2", doc_type="doc", body={"query": { "terms": {"_id": [_id]}}})
+	print res
+	alarm = res["hits"]["hits"][0]["_source"]
+	group = json.loads(alarm)
+	gList = group["members"]
+	for i in range(len(gList)):
+	    user = es.search(index="token", doc_type="doc", body={"query": { "match": {"userId": gList[0] }}})
+	    registration_id = user["hits"]["hits"][0]["_source"]["token"]
+	    message_title = "DELETE"
+	    message_body = "You have a group alarm update"
+	    data_message = _id
+	    result = push_service.notify_single_device(registration_id=registration_id,
+	    message_title=message_title, message_body=message_body, data_message=data_message)
+	print _id
+	res = es.delete(index="group", doc_type="doc", id=_id)
+	return jsonify(res) 
 
 app.run(host="45.56.125.90", port=5000, threaded=True)
